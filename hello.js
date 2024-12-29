6e9d6acca5498ed658aaefd2fcc547339875dfb3
Aakhya Chhauhan <142525613+AaakhyaChhauhan@users.noreply.github.com>
2024-12-29 11:43:21 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,28 +1,31 @@
     <!DOCTYPE html>
     <html lang="en">
-    <head>
+    <head>      
     <title>HTML CSS REVIEW</title>
     <style>
     .blue-button{
     background-color: blue;                      <!-- css included here
     border: 0cqh;
     color: aqua;
-
+        /*THIS IS A COMMENT IN CSS*/
     }
     .aqua-button{
 
     background-color: aqua;
     }
     </style>
-
     </head>
     <body>
-    <button title="GOODJOB" class="blue-button">      <!-- title one will be showed after we hover up the button
+    <button title="GOODJOB" class="blue-button"> 
+        onclick="                                  
+          alert('hello');          // title one will be showed after we hover up the button(COMMENT IN JAVASCRIPT)
+        ">
     hello
     </button>
-    <p>paragraph of <button class="aqua-button">hello</button> text</p>   <!--this is the method to write comment and the process of side code is known as nesting-->
+    <p>paragraph of <button class="aqua-button">hello</button> text</p>   // process of side code is known as nesting
     <script>
-    alert("hello");                            <!-- javascript here
+            console.log("
+    alert("hello");                            // javascript here
     </script>
 
 </body>
